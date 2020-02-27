7dc71ba249e85351699f23f4a5bdc6fc95ba509b
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2020-02-27 11:19:45 +0900

    Ver.0.2
    Update

--- chat.php
+++ chat.php
@@ -6,7 +6,6 @@ $save2_file = 'bbb.dat';
 date_default_timezone_set('Asia/Tokyo');
 header("Access-Control-Allow-Origin: *"); // CORS対策
 
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['b_send'])) {
   $put_data = htmlspecialchars($_POST['b_send'], ENT_QUOTES, "UTF-8");
 /*  $push_data = array("data" => array(
@@ -15,9 +14,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['b_send'])) {
   $push_data2 = $put_data."\t".date('Y-m-d H:i:s')."\t".$_SERVER["REMOTE_ADDR"]."\n";
   file_put_contents($save_file, $push_data, FILE_APPEND | LOCK_EX);
   file_put_contents($save2_file, $push_data2, FILE_APPEND | LOCK_EX);
-  echo file_get_contents($save_file);
+  if(isset($_COOKIE["last_date"])) {
+    // if($_COOKIE["last_date"] === filemtime($save_file)) {
+    if($_COOKIE["last_date"] == date("ymdHis",filemtime($save_file))) {
+        echo 'B';
+    } else {
+      echo file_get_contents($save_file);
+    }
+  } else {
+    echo file_get_contents($save_file);
+    // setcookie("last_date",filemtime($save_file),time()+60*60*24*365); //有効期限は1年
+    $set_time = date("ymdHis",filemtime($save_file));
+    setcookie("last_date",$set_time);
+  }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['b_req'])) {
-  echo file_get_contents($save_file);
+  if(isset($_COOKIE["last_date"]) && $_POST['b_req'] != 'BrainF*ck') { // 前の更新日時と同じとき、再送しない
+    if($_COOKIE["last_date"] == date("ymdHis",filemtime($save_file))) {
+      echo 'B';
+    } else {
+      echo file_get_contents($save_file);
+    }
+  } else {
+    echo file_get_contents($save_file);
+    // setcookie("last_date","",time()-60*60*24*365);
+    $set_time = date("ymdHis",filemtime($save_file));
+    // setcookie("last_date",$set_time,time()+60*60*24*365); //有効期限は1年
+    setcookie("last_date",$set_time);
+  }
 }
 /*
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['req'])) { //  POSTでreqが指定されている場合続行
