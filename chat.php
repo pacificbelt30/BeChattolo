d21a453dfa5053a66f7b970b2b03dbbae1c6647e
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2020-02-27 11:20:46 +0900

    Ver.0.3.1
    Update

--- chat.php
+++ chat.php
@@ -14,32 +14,32 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['b_send'])) {
   $push_data2 = $put_data."\t".date('Y-m-d H:i:s')."\t".$_SERVER["REMOTE_ADDR"]."\n";
   file_put_contents($save_file, $push_data, FILE_APPEND | LOCK_EX);
   file_put_contents($save2_file, $push_data2, FILE_APPEND | LOCK_EX);
-  if(isset($_COOKIE["last_date"])) {
+  if(isset($_POST["last_date"])) {
     // if($_COOKIE["last_date"] === filemtime($save_file)) {
-    if($_COOKIE["last_date"] == date("ymdHis",filemtime($save_file))) {
+/*    if($_POST["last_date"] == date("ymdHis",filemtime($save_file))) {
         echo 'B';
-    } else {
-      echo file_get_contents($save_file);
-    }
+    } else {*/
+      echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
+/*    } */
   } else {
-    echo file_get_contents($save_file);
+    echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
     // setcookie("last_date",filemtime($save_file),time()+60*60*24*365); //有効期限は1年
     $set_time = date("ymdHis",filemtime($save_file));
-    setcookie("last_date",$set_time);
+    setcookie("last_date",$set_time,time()+60*60*24*7);
   }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['b_req'])) {
-  if(isset($_COOKIE["last_date"]) && $_POST['b_req'] != 'bbb') { // 前の更新日時と同じとき、再送しない
-    if($_COOKIE["last_date"] == date("ymdHis",filemtime($save_file))) {
+  if(isset($_POST["last_date"]) && $_POST['b_req'] != 'bbb') { // 前の更新日時と同じとき、再送しない
+    if($_POST["last_date"] == date("ymdHis",filemtime($save_file))) {
       echo 'B';
     } else {
-      echo file_get_contents($save_file);
+      echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
     }
   } else {
-    echo file_get_contents($save_file);
+    echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
     // setcookie("last_date","",time()-60*60*24*365);
-    $set_time = date("ymdHis",filemtime($save_file));
+    // $set_time = date("ymdHis",filemtime($save_file));
     // setcookie("last_date",$set_time,time()+60*60*24*365); //有効期限は1年
-    setcookie("last_date",$set_time);
+    // setcookie("last_date",$set_time,time()+60*60*24*7);
   }
 }
 /*
