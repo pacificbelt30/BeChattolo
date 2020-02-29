6ec854e28dcd4e04fd3ef245faa77e2451359a8e
Fukuda-B <60131202+Fukuda-B@users.noreply.github.com>
2020-03-01 01:06:33 +0900

    Ver.0.5
    Migrated from Ajax to Comet
    CSS changes

--- chat.php
+++ chat.php
@@ -1,52 +1,96 @@
 <?php
+/*
+Beちゃっとぉ
+© 2020 Fukuda-B/Dakhuf
+
+// ----- 予定 -----
+チャットにユーザー名を入力するようにする
+改行コードに対応する
+アプリ版を作る
+コマンドに対応する
+通知 (設定できるようにする)
+
+// ----- コマンドなど -----
+
+From: Youtube
+太字 *太字*
+_斜体_
+-取り消し線-
+:***: //***部分に文字列で隠し絵文字に自動変換される
+
+From: Markdownチートシート
+```ソースコード```
+# これはH1タグ
+## これはH2タグ
+
+// ----- 更新履歴 -----
+Ver.0.5
+Cometによって効率化
+
+Ver.0.4
+ショートカット送信対応
+URL自動リンク化
+
+~Ver.0.3
+Ajaxで基本的なチャット
+*/
+
+// ----- PHPini設定 -----
+ini_set("max_execution_time",240); // 最大タイムアウト時間の指定
+// ----- 定数定義 -----
+define("HOLDING_TIMER", "180"); // リクエストの保持ループ回数
+define("TIMER_DENOMINATOR", "1"); // 更新チェック実行間隔(s)
+//>> (HOLDING_TIMER×TIMER_DENOMINATOR≠保持時間(s))
+
+// ----- 変数 -----
+
 $save_file = 'bbs.txt';
 $save2_file = 'bbb.dat';
+$break_flag = 0;
 
 // タイムゾーン指定 (Asia/Tokyo)
 date_default_timezone_set('Asia/Tokyo');
 header("Access-Control-Allow-Origin: *"); // CORS対策
 
+// ----- メイン処理 -----
+// ----- メッセージがPOSTされたとき -----
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['b_send'])) {
   $put_data = htmlspecialchars($_POST['b_send'], ENT_QUOTES, "UTF-8");
-/*  $push_data = array("data" => array(
-  )) */
   $push_data = $put_data."\t".date('Y-m-d H:i:s')."\n";
   $push_data2 = $put_data."\t".date('Y-m-d H:i:s')."\t".$_SERVER["REMOTE_ADDR"]."\n";
-  file_put_contents($save_file, $push_data, FILE_APPEND | LOCK_EX);
-  file_put_contents($save2_file, $push_data2, FILE_APPEND | LOCK_EX);
+  file_put_contents($save_file, $push_data, FILE_APPEND);
+  file_put_contents($save2_file, $push_data2, FILE_APPEND);
   if(isset($_POST["last_date"])) {
-    // if($_COOKIE["last_date"] === filemtime($save_file)) {
-/*    if($_POST["last_date"] == date("ymdHis",filemtime($save_file))) {
-        echo 'B';
-    } else {*/
       echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
-/*    } */
   } else {
     echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
-    // setcookie("last_date",filemtime($save_file),time()+60*60*24*365); //有効期限は1年
     $set_time = date("ymdHis",filemtime($save_file));
     setcookie("last_date",$set_time,time()+60*60*24*7);
   }
+// ----- メッセージがREQUESTされたとき -----
 } elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['b_req'])) {
   if(isset($_POST["last_date"]) && $_POST['b_req'] != 'bbb') { // 前の更新日時と同じとき、再送しない
-    if($_POST["last_date"] == date("ymdHis",filemtime($save_file))) {
+    // サーバでリクエストの保持
+    $rec_date = $_POST["last_date"];
+    for($i=0; $i<HOLDING_TIMER; $i++) { // REQUESTの保持ループ
+      clearstatcache(false, $save_file); // ファイルのステータスのキャッシュをクリア
+      $file_date = date("ymdHis",filemtime($save_file));
+      // echo $file_date."\n".$rec_date."\n";
+      if($rec_date !== $file_date) { // 更新があったら内容出力
+        $break_flag= 1;
+          break;
+      }
+      sleep(TIMER_DENOMINATOR);
+    }
+    if($break_flag===0) {
       echo 'B';
     } else {
       echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
     }
   } else {
+    // 初回ロード時など、即レスポンスする
     echo date("ymdHis",filemtime($save_file))."\n".file_get_contents($save_file);
-    // setcookie("last_date","",time()-60*60*24*365);
-    // $set_time = date("ymdHis",filemtime($save_file));
-    // setcookie("last_date",$set_time,time()+60*60*24*365); //有効期限は1年
-    // setcookie("last_date",$set_time,time()+60*60*24*7);
   }
 }
-/*
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['req'])) { //  POSTでreqが指定されている場合続行
-  if ($_POST['req'] == 'B!' && isset($_POST['call'])){ // データリクエスト時
-    echo_data(); // 出力
-  } elseif ($_POST['req' == 'Bee' && isset())
-}
-*/
+
 ?>
---
+++ main.js
@@ -0,0 +1,167 @@
+// Beちゃっとぉ クライアント側
+
+// ----- Javascript部分 -----
+
+//----- 定数定義 -----
+const CONTTT = document.getElementById('conttt'); // メッセージ内容の表示部分
+const TIME_B = document.getElementById('time_b'); // 時刻表示用(仮)
+const XHR_TIMEOUT = 1000 * 62; // サーバリクエストのタイムアウト時間(ms)
+const MAINLOOP_TIMER = 200; // メイン関数の実行間隔の時間 (ms)
+// const SEND_SERVER = 'https://u2net.azurewebsites.net/chat/chat.php'; // POSTするサーバURL
+const SEND_SERVER = 'chat.php';
+
+// ----- 変数定義 -----
+let s_cnt = 0; // 処理カウント用
+let last_date = 0; // 前回更新日時
+let dis_update = 0; // 更新するかしないかのフラグ
+let push_timer = 500; // Push通知の表示時間(ms)
+
+function nowD() {
+  const DATE = new Date();
+  let now_year = DATE.getFullYear();
+  let now_month = DATE.getMonth() + 1;
+  let now_date = DATE.getDate();
+  let now_hours = DATE.getHours();
+  let now_minute = DATE.getMinutes();
+  let now_sec = DATE.getSeconds();
+  return '' + now_year + now_month + now_date + now_hours + now_hours + now_minute + now_sec;
+}
+
+
+// ----- 初期処理 -----
+console.log('%cＢｅちゃっとぉ%c Ver.0.4.3 20200229', 'color: #fff; font-size: 2em; font-weight: bold;', 'color: #00a0e9;');
+
+// ----- メイン処理 -----
+document.addEventListener("DOMContentLoaded", function main() { // ロード時開始
+  console.log('%cSessionBegin %c> ' + nowD(), 'color: orange;', 'color: #bbb;');
+  const b_req = new XMLHttpRequest();
+  b_req.open('POST', SEND_SERVER, true);
+  b_req.setRequestHeader('Pragma', 'no-cache'); // キャッシュを無効にするためのヘッダ指定
+  b_req.setRequestHeader('Cache-Control', 'no-cach');
+  b_req.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+  b_req.timeout = XHR_TIMEOUT; // サーバリクエストのタイムアウト時間の指定
+  if (s_cnt == 0) {
+    b_req.send('b_req=bbb&last_date=' + last_date); // b_req=bbbを指定することで更新日時の判定なしで、即レスポンスを行い、データを取得します
+  } else {
+    b_req.send('b_req=BBBBB&last_date=' + last_date); // b_req≠bbbの時は、ファイルの更新日時による判定で、更新がある場合のみ取得します
+  }
+  b_req.onreadystatechange = function () { // 通信ステータスが変わったとき実行
+    if (b_req.readyState === 4) {
+      if (b_req.status === 200) {
+        // b_data = b_post.responseText.parse(json);
+        let out_data = AppAdjust(b_req.responseText);
+        if (dis_update == 0) { // dis_update == 0 の時にメッセージ内容の表示の更新を行います
+          CONTTT.innerHTML = AutoLink(out_data);
+          if (s_cnt !== 0) { // 初回読み込み時以外で、更新があった場合はPush通知を行う
+            push_notice(); // Push通知を行う
+            document.title = '🟧Beちゃっとぉ';
+          }
+        }
+        s_cnt++;
+        console.log(s_cnt);
+        // main();
+        setTimeout(main, MAINLOOP_TIMER);
+      // console.log(b_data);
+      } else {
+        setTimeout(main, XHR_TIMEOUT);
+      }
+    }
+  }
+  // console.log('>');
+  // TIME_B.innerHTML = nowD();
+  // setTimeout(main, MAINLOOP_TIMER);
+});
+
+// ----- サーバにメッセージ内容を送信する関数 -----
+div_top = document.getElementById('chat_content');
+
+function b_send() { // データをサーバに送信する関数
+  var send_data = div_top.value; // inputに入っている値を$send_dataに代入します
+  if (send_data.length >= 1011 || send_data.length <= 0) { // データサイズのチェックです
+    console.log("POST_SIZE > OVER <"); // データサイズが大きすぎる場合は拒否
+    return B;
+  } else { // 以下main関数とほぼ同様
+    console.log(send_data);
+    div_top.value = '';
+    var b_post = new XMLHttpRequest();
+    b_post.open('POST', SEND_SERVER, true);
+    b_post.setRequestHeader('Pragma', 'no-cache');
+    b_post.setRequestHeader('Cache-Control', 'no-cach');
+    b_post.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    b_post.timeout = XHR_TIMEOUT; // サーバリクエストのタイムアウト時間の指定
+    b_post.send('b_send=' + send_data);
+    b_post.onreadystatechange = function () {
+      if (b_post.readyState === 4) {
+        if (b_post.status === 200) {
+          CONTTT.innerHTML = AutoLink(AppAdjust(b_post.responseText));
+          console.log('POST_OK!');
+        }
+      }
+    }
+  }
+}
+
+// ----- メッセージ内容表示用にHTMLタグ付けする関数 -----
+function AppAdjust(OriginalText) {
+  if (OriginalText == 'B') { // 'B'が渡された場合は、ファイルの更新はありません
+    OriginalText = sessionStorage.getItem('receive_data'); // 更新がない場合、SessionStorageからデータを取得します
+    dis_update = 1; // dip_update = 1 の時は、メッセージ内容の更新を行いません
+    // console.log('session');
+  } else {
+    sessionStorage.setItem('receive_data', OriginalText);
+    dis_update = 0;
+  }
+  var con_b_data = OriginalText.split(/\r?\n/g); // 改行で区切り、配列にします
+  last_date = con_b_data[0]; // 配列の最初はファイルの更新日時が入っています
+  for (var i = 1; i < con_b_data.length; i++) { // Tab区切りで配列にし、HTMLタグを加えます
+    var arr_b_data = con_b_data[i].split(/\t/);
+    con_b_data[i] = arr_b_data[0] + "<span id=date>" + arr_b_data[1] + "</span>";
+  }
+  var out_data = '';
+  for (var i = 1; i < con_b_data.length - 1; i++) { // メッセージ内容の表示部分に出力するために順番を入れ替え、HTMLタグを加えます
+    if (i == con_b_data.length - 2) {
+      out_data = "<li id=list2>" + con_b_data[i] + "</li>" + out_data;
+    } else {
+      out_data = "<li id=list>" + con_b_data[i] + "</li>" + out_data;
+    }
+  }
+  return out_data;
+}
+
+// ----- サーバにリクエストを送る関数 -----
+// functoin req(get_mode, )
+
+// ----- キー入力を処理する関数 -----
+// Alt+Enter(keyCode==13)が入力されたとき、b_send()を実行
+document.onkeydown = keydown;
+
+function keydown() {
+  if (event.altKey == true && event.keyCode == 13) { // Alt + Enter で送信
+    b_send();
+  }
+}
+
+// ----- Push通知を行う関数 -----
+function push_notice(message, timer) {
+  if (!message) {
+    message = 'New message received!';
+  }
+  Push.create(message, {
+    timeout: timer,
+    onClick: function () {
+      window.focus();
+      this.close();
+    }
+  });
+
+}
+
+// ----- 自動リンク化する関数 -----
+// $strに入れると、リンク部分が<a>で囲われてreturn
+function AutoLink(str) {
+  var regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g; // ']))/;
+  var regexp_makeLink = function (all, url, h, href) {
+    return '<a href="h' + href + '"  target="_blank">' + url + '</a>';
+  }
+  return str.replace(regexp_url, regexp_makeLink);
+}
